9a09473bbdb28449450c511cfd9dddda4466416e
Neelima <neelimanama@gmail.com>
2022-07-21 11:50:19 +0100

    jira 123 - committing project for frist time

--- src/test/java/App_Tests.java
+++ src/test/java/App_Tests.java
@@ -1,10 +1,13 @@
 import org.testng.annotations.Test;
 
+import java.sql.SQLOutput;
+
 public class App_Tests {
     @Test
     public void test1(){
 
         System.out.println("Running test");
         System.out.println("running test1");
+        System.out.println("running test2");
     }
 }
